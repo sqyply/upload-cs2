556d4e9ab4555dc5a08c7f41180a6d9827e9bcf5
sqyply <sqyply@gmail.com>
2026-04-03 18:36:39 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "CS2 Upgrader",
+  description: "Upgrade your CS2 skins",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
